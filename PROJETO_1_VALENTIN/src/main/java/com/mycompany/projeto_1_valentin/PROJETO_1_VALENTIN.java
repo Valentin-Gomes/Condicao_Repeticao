4cf92244ff3d43eb0187b93d6d2fef44653d081d
Valentin Gomes <valentin_gomes@hotmail.com>
2024-08-14 21:35:20 -0300

    Alteracao 1

--- PROJETO_1_VALENTIN/src/main/java/com/mycompany/projeto_1_valentin/PROJETO_1_VALENTIN.java
+++ PROJETO_1_VALENTIN/src/main/java/com/mycompany/projeto_1_valentin/PROJETO_1_VALENTIN.java
@@ -1,16 +1,43 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+   Sistema de Aprovação de Alunos: Crie um programa que receba as notas de um aluno em
+   três disciplinas e calcule sua média. Utilize estruturas IF e ELSE para determinar se o aluno
+   foi aprovado, reprovado ou está em recuperação com base na média (Aprovado: média ≥ 7,
+   Recuperação: 5 ≤ média < 7, Reprovado: média < 5).
  */
 
 package com.mycompany.projeto_1_valentin;
 
-/**
- *
- * @author v.gomes
- */
+import java.util.Scanner;
 public class PROJETO_1_VALENTIN {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner ler = new Scanner(System.in);
+        
+        float media;
+        
+        System.out.println("Digite suas 3 notas para calcular a média");
+        System.out.print("Digite a primeira nota: ");
+        int notaUm = ler.nextInt();
+        
+        System.out.print("Digite a segunda nota: ");
+        int notaDois = ler.nextInt();
+        
+        System.out.print("Digite a terceira nota: ");
+        int notaTres = ler.nextInt();
+        
+        media = (notaUm + notaDois + notaTres) / 3;
+        
+        System.out.println("A média foi de " +media);
+        
+        if (media >= 7){
+            System.out.println("APROVADO!");
+        }
+        else if (media < 7 && media >=5){
+            System.out.println("RECUPERAÇÃO");
+        }
+        else{
+            System.out.println("REPROVADO");
+        }
+        
     }
 }
---
+++ PROJETO_2_VALENTIN/src/main/java/com/mycompany/projeto_2_valentin/PROJETO_2_VALENTIN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_2_valentin;
+
+/**
+ *
+ * @author v.gomes
+ */
+public class PROJETO_2_VALENTIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
